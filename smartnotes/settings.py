3ff5d329bbe38a75ed6cb76727ac0d598bfdce82
Harsh Navdhare <harsh.navdhare@proton.me>
2022-07-11 23:59:51 +0530

    Production changes
    Signed-off-by: Harsh Navdhare <harsh.navdhare@proton.me>

--- smartnotes/settings.py
+++ smartnotes/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-2kbm79pyl1zx_b!zoolvzr02^jt+re+qwp5c8t@d2-kvqz(yio"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
