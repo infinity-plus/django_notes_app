08a8d4052a54ca53cde9c6c1e9d17b829a073c49
Harsh Navdhare <harsh.navdhare@proton.me>
2022-07-11 16:00:17 +0530

    Add Delete endpoint
    Signed-off-by: Harsh Navdhare <harsh.navdhare@proton.me>

--- notes/urls.py
+++ notes/urls.py
@@ -3,8 +3,29 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.NoteListView.as_view(), name="list_notes"),
-    path("<int:pk>/", views.NoteDetailView.as_view(), name="note_detail"),
-    path("<int:pk>/edit", views.NoteUpdateView.as_view(), name="note_update"),
-    path("new/", views.NoteCreateView.as_view(), name="note_new"),
+    path(
+        "",
+        views.NoteListView.as_view(),
+        name="list_notes",
+    ),
+    path(
+        "<int:pk>/",
+        views.NoteDetailView.as_view(),
+        name="note_detail",
+    ),
+    path(
+        "<int:pk>/edit",
+        views.NoteUpdateView.as_view(),
+        name="note_update",
+    ),
+    path(
+        "<int:pk>/delete",
+        views.NoteDeleteView.as_view(),
+        name="note_delete",
+    ),
+    path(
+        "new/",
+        views.NoteCreateView.as_view(),
+        name="note_new",
+    ),
 ]
--- notes/views.py
+++ notes/views.py
@@ -1,4 +1,10 @@
-from django.views.generic import ListView, DetailView, CreateView, UpdateView
+from django.views.generic import (
+    ListView,
+    DetailView,
+    CreateView,
+    UpdateView,
+)
+from django.views.generic.edit import DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin
 
 from .forms import NoteForm
@@ -25,6 +31,12 @@ class NoteUpdateView(LoginRequiredMixin, UpdateView):
     def form_valid(self, form):
         form.instance.author = self.request.user
         return super().form_valid(form)
+
+
+class NoteDeleteView(LoginRequiredMixin, DeleteView):
+    model = Note
+    success_url = "/notes/"
+    template_name = "notes/note_delete.html"
 
 
 class NoteListView(LoginRequiredMixin, ListView):
