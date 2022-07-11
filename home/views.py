f6f4638ae1180ee03008ef463088da359c93727e
Harsh Navdhare <harsh.navdhare@proton.me>
2022-07-11 21:57:42 +0530

    Add sign-up view
    Also fix up styling issues
    
    Signed-off-by: Harsh Navdhare <harsh.navdhare@proton.me>

--- home/views.py
+++ home/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+import django
+from django.shortcuts import redirect, render
 
 # Create your views here.
 from datetime import datetime
@@ -6,6 +7,21 @@ from django.contrib.auth.decorators import login_required
 from django.views.generic import TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.views import LoginView, LogoutView
+from django.views.generic.edit import CreateView
+from django.contrib.auth.forms import UserCreationForm
+
+
+class SignupView(CreateView):
+    form_class = UserCreationForm
+    success_url = "/login/"
+    template_name = "home/signup.html"
+
+    def get(self, request, *args, **kwargs):
+        return (
+            redirect("list_notes")
+            if request.user.is_authenticated
+            else super().get(request, *args, **kwargs)
+        )
 
 
 class LoginInterfaceView(LoginView):
