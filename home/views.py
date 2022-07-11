3698eced80ee42e14f4feccacac29d08febd1f7c
Harsh Navdhare <harsh.navdhare@proton.me>
2022-07-11 23:57:41 +0530

    Remove unused imports
    Signed-off-by: Harsh Navdhare <harsh.navdhare@proton.me>

--- home/views.py
+++ home/views.py
@@ -1,14 +1,11 @@
-import django
-from django.shortcuts import redirect, render
-
-# Create your views here.
 from datetime import datetime
-from django.contrib.auth.decorators import login_required
-from django.views.generic import TemplateView
+
+from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.views import LoginView, LogoutView
+from django.shortcuts import redirect
+from django.views.generic import TemplateView
 from django.views.generic.edit import CreateView
-from django.contrib.auth.forms import UserCreationForm
 
 
 class SignupView(CreateView):
